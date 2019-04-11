e8767fe57686f800769b85271de786cded941368
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-04-11 23:59:53 +0900

    fix create.sh

--- scripts/create.sh
+++ scripts/create.sh
@@ -9,7 +9,7 @@ set -e
 # Create a Cloudformation stack from the local template `cloudformation-vpc.yaml`
 VPC_STACK_NAME="bench-vpc"
 SSH_LOCATION="$(curl ifconfig.co 2> /dev/null)/32"
-INSTANCE_TYPE="t2.large"
+INSTANCE_TYPE="t2.micro"
 
 aws cloudformation create-stack \
   --stack-name "${VPC_STACK_NAME}" \
@@ -20,33 +20,38 @@ aws cloudformation create-stack \
 echo "Waiting until the Cloudformation stack is CREATE_COMPLETE"
 aws cloudformation wait stack-create-complete --stack-name "${VPC_STACK_NAME}"
 
+# Variables to be passed upon EC2 creation in the next step
 DESCRIBED=$(aws cloudformation describe-stacks --stack-name "${VPC_STACK_NAME}")
-IAM_INSTANCEPROFILE_SSM=$DESCRIBED
+SUBNET=$(echo $DESCRIBED | jq -c '.Stacks[0].Outputs[] | select(.OutputKey == "Subnet") | .OutputValue')
+SECURITY_GROUP=$(echo $DESCRIBED | jq -c '.Stacks[0].Outputs[] | select(.OutputKey == "SecurityGroup") | .OutputValue')
+IAM_INSTANCEPROFILE_SSM=$(echo $DESCRIBED | jq -c '.Stacks[0].Outputs[] | select(.OutputKey == "InstanceProfile") | .OutputValue')
 
+# Create EC2 instances. Since CloudFormation doesn't support creation of a variable number of EC2 instances,
+# you need to create the instances via CLI.
 # If you are using a command line tool, base64-encoding is performed for you, and you can load the text from a file., https://docs.aws.amazon.com/cli/latest/reference/ec2/run-instances.html
-aws run-instances \
+aws ec2 run-instances \
   --image-id "ami-0d7ed3ddb85b521a6" \
   --instance-type "${INSTANCE_TYPE}" \
   --key-name "demo-key-pair" \
-  --iam-instance-profile "${IAM_INSTANCEPROFILE_SSM}" \
+  --iam-instance-profile "Name=${IAM_INSTANCEPROFILE_SSM}" \
   --user-data "file://user-data.sh" \
-  --network-interfaces "AssociatePublicIpAddress=true,DeviceIndex=0,PrivateIpAddress=10.0.0.5,SubnetId=" \
-  --tag-specifications 'ResourceType=instance,Tags=[{Key=role,Value=frontend}]'
+  --network-interfaces "AssociatePublicIpAddress=true,DeviceIndex=0,PrivateIpAddress=10.0.0.5,Groups=${SECURITY_GROUP},SubnetId=${SUBNET}" \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=role,Value=frontend}]"
 
-# aws run-instances \
+#aws ec2 run-instances \
 #  --image-id "ami-0d7ed3ddb85b521a6" \
 #  --instance-type "${INSTANCE_TYPE}" \
 #  --key-name "demo-key-pair" \
-#  --iam-instance-profile \
+#  --iam-instance-profile "Name=${IAM_INSTANCEPROFILE_SSM}" \
 #  --user-data "file://user-data.sh" \
-#  --network-interfaces "AssociatePublicIpAddress=true,DeviceIndex=0,PrivateIpAddress=10.0.0.5,SubnetId=" \
-#  --tag-specifications 'ResourceType=instance,Tags=[{Key=role,Value=backend}]'
+#  --network-interfaces "AssociatePublicIpAddress=true,DeviceIndex=0,PrivateIpAddress=10.0.0.5,Groups=${SECURITY_GROUP},SubnetId=${SUBNET}" \
+#  --tag-specifications "ResourceType=instance,Tags=[{Key=role,Value=backend}]"
 
-#aws run-instances \
+#aws ec2 run-instances \
 #  --image-id "ami-0d7ed3ddb85b521a6" \
 #  --instance-type "${INSTANCE_TYPE}" \
 #  --key-name "demo-key-pair" \
-#  --iam-instance-profile \
+#  --iam-instance-profile "Name=${IAM_INSTANCEPROFILE_SSM}" \
 #  --user-data "file://user-data.sh" \
-#  --network-interfaces "AssociatePublicIpAddress=true,DeviceIndex=0,PrivateIpAddress=10.0.0.5,SubnetId=" \
-#  --tag-specifications 'ResourceType=instance,Tags=[{Key=role,Value=wrk-client}]'
+#  --network-interfaces "AssociatePublicIpAddress=true,DeviceIndex=0,PrivateIpAddress=10.0.0.5,Groups=${SECURITY_GROUP},SubnetId=${SUBNET}" \
+#  --tag-specifications "ResourceType=instance,Tags=[{Key=role,Value=wrk}]"
