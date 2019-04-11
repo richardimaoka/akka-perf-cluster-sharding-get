7a0611c608894a795185ba74830a1913b6d2fe33
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-04-12 00:46:59 +0900

    Fix docker files

--- deploy/user-data.sh
+++ deploy/user-data.sh
@@ -7,6 +7,6 @@ yum -y install git
 cd /home/ec2-user
 git clone https://github.com/richardimaoka/akka-perf-cluster-sharding-get
 cd akka-perf-cluster-sharding-get
-docker build Dockerfile-backend  -t richard-perf-backend:latest
-docker build Dockerfile-frontend -t richard-perf-frontend:latest
-docker build Dockerfile-datagen  -t richard-perf-frontend:latest
+docker build -f Dockerfile-backend  -t richard-perf-backend:latest  .
+docker build -f Dockerfile-frontend -t richard-perf-frontend:latest .
+docker build -f Dockerfile-datagen  -t richard-perf-frontend:latest .
