a613243813c4acbe3ab37dc054b7155fc96d91d2
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-04-12 23:29:20 +0900

    fix docker run commands

--- ssm-scripts/frontend.sh
+++ ssm-scripts/frontend.sh
@@ -2,5 +2,6 @@
 
 docker run \
   -e HOST_IPV4_ADDRESS="$(curl http://169.254.169.254/latest/meta-data/local-ipv4)" \
-  -e AKKA_CLUSTER_SEED_NODES="10.0.0.5" \ # See https://github.com/lightbend/config/issues/163
-  richard-perf-frontend:latest
+  -e AKKA_CLUSTER_SEED_NODE_IPV4="10.0.0.5" \ # See https://github.com/lightbend/config/issues/163
+  --network host # to make EC2 IP Address same as Container's IP address
+  -d richard-perf-frontend:latest
