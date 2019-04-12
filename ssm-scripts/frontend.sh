713e11b3978abf52b953faac83a74fd7a6263287
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-04-12 23:45:20 +0900

    Fix port usage

--- ssm-scripts/frontend.sh
+++ ssm-scripts/frontend.sh
@@ -2,6 +2,7 @@
 
 docker run \
   -e HOST_IPV4_ADDRESS="$(curl http://169.254.169.254/latest/meta-data/local-ipv4)" \
-  -e AKKA_CLUSTER_SEED_NODE_IPV4="10.0.0.5" \ # See https://github.com/lightbend/config/issues/163
+  -e AKKA_CLUSTER_SEED_NODE_IPV4="10.0.0.105" \ # See https://github.com/lightbend/config/issues/163
+  -e AKKA_CLUSTER_SEED_NODE_PORT="2551" \
   --network host # to make EC2 IP Address same as Container's IP address
   -d richard-perf-frontend:latest
