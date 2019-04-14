c7a98cd8a5831d324abaa11883e29476e26c6eb8
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-04-15 00:06:29 +0900

    fix dockerfile

--- scripts/remote/wrk-client.sh
+++ scripts/remote/wrk-client.sh
@@ -56,10 +56,7 @@ do
 done
 
 # Run wrk
-# Using the --network host to reach out to other EC2 instances via their EC2 IPV4
-# and mounting the current directory to wrk container's WORKDIR = '/data'
-# Note that this mounting doesn't work on WSL Docker: see https://nickjanetakis.com/blog/setting-up-docker-for-windows-and-wsl-to-work-flawlessly#ensure-volume-mounts-work
-WRK_CMD="docker run --network host -v $(pwd):/data williamyeh/wrk -t ${threads} -c ${connections} -d ${duration} -s wrk_script.lua ${TARGET_URL}"
+WRK_CMD="wrk -t ${threads} -c ${connections} -d ${duration} -s wrk_script.lua ${TARGET_URL}"
 echo "running:"
 echo "${WRK_CMD}"
 ${WRK_CMD}
