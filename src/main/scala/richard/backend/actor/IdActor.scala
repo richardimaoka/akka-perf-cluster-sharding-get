ce1b66d71f6a9318f14465f734ec2d1a711123f9
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-04-23 04:47:39 +0900

    logging

--- src/main/scala/richard/backend/actor/IdActor.scala
+++ src/main/scala/richard/backend/actor/IdActor.scala
@@ -2,22 +2,30 @@ package richard.backend.actor
 
 import akka.actor.Actor
 import akka.cluster.sharding.ShardRegion
+import akka.event.Logging
 
 class IdActor extends Actor {
   import richard.backend.actor.IdActor._
+  val log = Logging(context.system, this)
   var _id: String = ""
+
+  log.info(s"Initialized IdActor")
 
   def receiveUninitialized: Receive = {
     case Create(i) =>
+      log.info(s"Create(${i}) is received by ${self.path}. Changing Actor's `receive` to receiveInitialized")
       _id = i
       context.become(receiveInitialized)
       sender() ! "Created"
-    case _ =>
+    case msg =>
+      log.info(s"Unexpected message ${msg} is received by ${self.path}. So stopping this Actor itself.")
       context.stop(self)
   }
 
   def receiveInitialized: Receive = {
-    case Get(_) => sender() ! _id
+    case Get(i) =>
+      log.info(s"Get(${i}) is received by ${self.path}. Returning the ${_id} back to the sender")
+      sender() ! _id
   }
 
   def receive: Receive = receiveUninitialized
